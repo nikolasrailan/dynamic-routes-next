79d341bea8dd6f993cb138bfdf01c54565f20d3a
Nikolas <nikolasrailanmp@gmail.com>
2024-10-18 10:23:23 -0300

    mostrando links dos cursos

--- src/api/cursos.ts
+++ src/api/cursos.ts
@@ -13,5 +13,26 @@ type Aula = {
   nome: string;
   descricao: string;
   total_aula: number;
-  total_horas: number;
+  tempo: number;
+  curso_id: number;
+  order: number;
 };
+
+export async function getCursos() {
+  const response = await fetch("https://api.origamid.online/cursos");
+  return (await response.json()) as Curso[];
+}
+
+export async function getCurso(curso: string) {
+  const response = await fetch(`https://api.origamid.online/cursos/${curso}`);
+  return (await response.json()) as Curso & {
+    aulas: Aula[];
+  };
+}
+
+export async function getAula(curso: string, aula: string) {
+  const response = await fetch(
+    `https://api.origamid.online/cursos/${curso}/aula`
+  );
+  return (await response.json()) as Aula;
+}
--- src/app/cursos/[curso]/page.tsx
+++ src/app/cursos/[curso]/page.tsx
@@ -1,3 +1,5 @@
+import { getCurso } from "@/api/cursos";
+
 type PageParams = {
   params: {
     curso: string;
@@ -5,10 +7,12 @@ type PageParams = {
 };
 
 export default async function CursoPage({ params }: PageParams) {
+  const curso = await getCurso(params.curso);
+
   return (
     <div>
       <main>
-        <h1>Curso: {params.curso}</h1>
+        <h1>{curso.nome}</h1>
       </main>
     </div>
   );
